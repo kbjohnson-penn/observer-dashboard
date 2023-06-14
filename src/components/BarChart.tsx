71f85f5bd25e1abcbc6d9fc041689a4a26a7b5aa
Alexander Budko <103473998+alex-budko@users.noreply.github.com>
2023-06-14 13:35:24 -0400

    refactor

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import { Bar } from "react-chartjs-2";
+import { ChartOptions, ChartData } from "chart.js";
+import { Card, WrapItem } from "@chakra-ui/react";
+
+interface BarChartProps {
+  options: ChartOptions;
+  data: ChartData<"bar">;
+}
+
+const BarChart: FC<BarChartProps> = ({ options, data }) => {
+  return (
+    <WrapItem>
+      <Card>
+        <Bar options={options} data={data} />;
+      </Card>
+    </WrapItem>
+  );
+};
+
+export default BarChart;
--- src/layout/DataSide.tsx
+++ src/layout/DataSide.tsx
@@ -8,7 +8,8 @@ import {
   WrapItem,
 } from "@chakra-ui/react";
 import { StatisticsTable } from "../components/StatisticsTable";
-import { Chart } from "react-chartjs-2";
+import BarChart from "../components/BarChart";
+import { chartOptions, chartData } from "../utils/chartConfig";
 import "chart.js/auto";
 
 const DataSide = () => {
@@ -19,86 +20,15 @@ const DataSide = () => {
     };
   });
 
-  const numCards = 0;
-  const chartOptions = {
-    title: {
-      display: true,
-      text: 'Chart Title',
-    },
-    scales: {
-      x: {
-        title: {
-          display: true,
-          text: 'X-Axis Title',
-        },
-      },
-      y: {
-        title: {
-          display: true,
-          text: 'Y-Axis Title',
-        },
-      },
-    },
-  };
-  const chartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-    datasets: [
-      {
-        label: '# of videos',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-      }
-    ]
-  };
-  
   return (
     <Center>
       <VStack spacing={4}>
-        <Wrap spacing="30px" justify="center">
-          {Array.from({ length: numCards }).map((_, i) => (
-            <WrapItem key={i}>
-              <Card
-                display="flex"
-                alignItems={"center"}
-                justifyContent="center"
-                w={"150px"}
-                h="150px"
-                boxShadow={"2xl"}
-                rounded={"2xl"}
-                p={4}
-              >
-                <CircularProgress
-                  isIndeterminate
-                  color="green.300"
-                />
-              </Card>
-            </WrapItem>
-          ))}
+        <Wrap>
+          <BarChart options={chartOptions} data={chartData} />
+          <BarChart options={chartOptions} data={chartData} />
+          <BarChart options={chartOptions} data={chartData} />
         </Wrap>
-        <Box>
-          <Chart
-            options={chartOptions}
-            data={chartData}
-            type={'bar'}
-          />
-          <StatisticsTable data={tableData} />
-        </Box>
+        <StatisticsTable data={tableData} />
       </VStack>
     </Center>
   );
